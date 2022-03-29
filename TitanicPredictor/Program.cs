7914da92722999539684c2943391f4508b94649d
wesmac <wesmac8989@gmail.com>
2022-03-29 18:32:33 -0300

    initial commit

---
+++ TitanicPredictor/Program.cs
@@ -0,0 +1,18 @@
+﻿using Spectre.Console;
+using TitanicPredictor;
+
+Titanic.ModelInput input = new Titanic.ModelInput
+{
+    Name = AnsiConsole.Ask<string>("Name:\n"),
+    Age = AnsiConsole.Ask<float>("Age:\n"),
+    Fare = AnsiConsole.Ask<float>("Fare Payed:\n"),
+    Parents_Children_Aboard = AnsiConsole.Ask<float>("Family Members Onboard:\n"),
+    Pclass = AnsiConsole.Ask<float>("Class:\n"),
+    Sex = AnsiConsole.Ask<string>("Sex:\n"),
+    Siblings_Spouses_Aboard = AnsiConsole.Ask<float>("Siblings Aboard:\n"),
+}; 
+
+var result = Titanic.Predict(input);
+
+string survived = result.Prediction == 1 ? "Survived" : "Died";
+Console.WriteLine(survived);
